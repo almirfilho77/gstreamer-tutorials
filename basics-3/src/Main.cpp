fb92c83e3cbde791cbc5d48037262b88bd8bc68d
Almir Filho <almir.filho77@gmail.com>
2023-04-05 21:00:17 -0400

    Implement exercice changes proposed in the tutorial
        - The function gst_element_get_static_pad did not work in this case, so I replaced it with gst_element_get_compatible_pad
        - That made me curious about the caps of the compatible pad, so I wrote a function to get the caps and show some information about it
        - When I tested the pad with the function gst_pad_has_current_caps it returned false for both pads (audio and video), and I don't know why this is

--- basics-3/src/Main.cpp
+++ basics-3/src/Main.cpp
@@ -5,13 +5,17 @@
 typedef struct _CustomData {
     GstElement *pipeline;
     GstElement *source;
-    GstElement *convert;
+    GstElement *video_convert;
+    GstElement *audio_convert;
     GstElement *resample;
-    GstElement *sink;
+    GstElement *video_sink;
+    GstElement *audio_sink;
 } CustomData;
 
 /* Handler for the pad-added signal */
 static void pad_added_handler(GstElement *src, GstPad *pad, CustomData *data);
+static bool check_error_elements_created(CustomData &data);
+static void retrive_pad_info(GstPad *pad);
 
 int 
 main(int argc, char* argv[])
@@ -23,64 +27,56 @@ main(int argc, char* argv[])
     gboolean terminate = FALSE;
 
     /* Initialize GStreamer */
+    std::cout << "Init gst\n";
     gst_init(&argc, &argv);
 
     /* Create the elements */
+    std::cout << "Create elements\n";
     data.source = gst_element_factory_make("uridecodebin", "source");
-    data.convert = gst_element_factory_make("audioconvert", "convert");
+    data.video_convert = gst_element_factory_make("videoconvert", "video_convert");
+    data.audio_convert = gst_element_factory_make("audioconvert", "audio_convert");
     data.resample = gst_element_factory_make("audioresample", "resample");
-    data.sink = gst_element_factory_make("autoaudiosink", "sink");
+    data.video_sink = gst_element_factory_make("autovideosink", "video_sink");
+    data.audio_sink = gst_element_factory_make("autoaudiosink", "audio_sink");
 
     /* Create the empty pipeline */
+    std::cout << "Create the empty pipeline\n";
     data.pipeline = gst_pipeline_new("test-pipeline");
 
     /* Test if everything went well with element creation */
-    bool err_detected = false;
-    if (data.pipeline == nullptr)
-    {
-        g_printerr("Pipeline element could not be created. \n");
-        err_detected = true;
-    }
-    if (data.source == nullptr)
-    {
-        g_printerr("Source element could not be created. \n");
-        err_detected = true;
-    }
-    if (data.convert == nullptr)
-    {
-        g_printerr("Convert element could not be created. \n");
-        err_detected = true;
-    }
-    if (data.resample == nullptr)
-    {
-        g_printerr("Resample element could not be created. \n");
-        err_detected = true;
-    }
-    if (data.sink == nullptr)
-    {
-        g_printerr("Sink element could not be created. \n");
-        err_detected = true;
-    }
-    if (err_detected) 
+    std::cout << "Test if everything went well with element creation\n";
+    if (check_error_elements_created(data) == TRUE)
         return -1;
 
     /* Build the pipeline. We are NOT linking the source at this
      * point. We will do it later. 
      */
+    std::cout << "Build the pipeline without link\n";
     gst_bin_add_many(GST_BIN(data.pipeline), 
-                    data.source, 
-                    data.convert, 
+                    data.source,
+                    data.video_convert,
+                    data.video_sink,
+                    data.audio_convert, 
                     data.resample,
-                    data.sink,
+                    data.audio_sink,
                     NULL);
 
-    gboolean retval = gst_element_link_many(data.convert, data.resample, data.sink, NULL);
+    gboolean retval = gst_element_link_many(data.video_convert, data.video_sink, NULL);
     if (retval == FALSE)
     {
-        g_printerr("Elements could not be linked.\n");
+        g_printerr("Elements in video chain could not be linked.\n");
         gst_object_unref(data.pipeline);
         return -1;
     }
+
+    retval = gst_element_link_many(data.audio_convert, data.resample, data.audio_sink, NULL);
+    if (retval == FALSE)
+    {
+        g_printerr("Elements audio chain could not be linked.\n");
+        gst_object_unref(data.pipeline);
+        return -1;
+    }
+    
 
     /* Set the URI to play */
     g_object_set(data.source, "uri", "https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm", NULL);
@@ -155,7 +151,7 @@ main(int argc, char* argv[])
 /* This is the definition of the callback funcion for when the pad is added */
 static void pad_added_handler(GstElement *src, GstPad *new_pad, CustomData *data)
 {
-    GstPad *sink_pad = gst_element_get_static_pad(data->convert, "sink");
+    GstPad *sink_pad = NULL;
     GstPadLinkReturn ret;
     GstCaps *new_pad_caps = NULL;
     GstStructure *new_pad_struct = NULL;
@@ -163,25 +159,47 @@ static void pad_added_handler(GstElement *src, GstPad *new_pad, CustomData *data
 
     g_print("Received new pad '%s' from '%s': \n", GST_PAD_NAME(new_pad), GST_ELEMENT_NAME(src));
 
-    /* If our converter is already linked, we have nothing to do here */
-    if (gst_pad_is_linked(sink_pad))
-    {
-        g_print("We are already linked. Ignoring...\n");
-        goto exit;
-    }
-
-    /* Check the new pad's type */
+    /* Check the new pad's type and Attempt the link */
     new_pad_caps = gst_pad_get_current_caps(new_pad);
     new_pad_struct = gst_caps_get_structure(new_pad_caps, 0);
     new_pad_type = gst_structure_get_name(new_pad_struct);
-    if (!g_str_has_prefix(new_pad_type, "audio/x-raw"))
+
+    if (g_str_has_prefix(new_pad_type, "video/x-raw"))
     {
-        g_print("It has type '%s' which is not raw audio. Ignoring...\n", new_pad_type);
+        std::cout << "Linking video pads\n";
+        sink_pad = gst_element_get_compatible_pad(data->video_convert, new_pad, new_pad_caps);
+
+        /* Try to get info on the compatible pad */
+        retrive_pad_info(sink_pad);
+
+        if (gst_pad_is_linked(sink_pad))
+        {
+            g_print("Video pad already linked. Ignoring...\n");
+            goto exit;
+        }
+        ret = gst_pad_link(new_pad, sink_pad);
+    }
+    else if (g_str_has_prefix(new_pad_type, "audio/x-raw"))
+    {
+        std::cout << "Linking audio pads\n";
+        sink_pad = gst_element_get_compatible_pad(data->audio_convert, new_pad, new_pad_caps);
+
+        /* Try to get info on the compatible pad */
+        retrive_pad_info(sink_pad);
+
+        if (gst_pad_is_linked(sink_pad))
+        {
+            g_print("Audio pad already linked. Ignoring...\n");
+            goto exit;
+        }
+        ret = gst_pad_link(new_pad, sink_pad);
+    }
+    else
+    {
+        g_print("It has type '%s' which is not necessary. Ignoring...\n", new_pad_type);
         goto exit;
     }
 
-    /* Attempt the link */
-    ret = gst_pad_link(new_pad, sink_pad);
     if (GST_PAD_LINK_FAILED(ret))
     {
         g_print("Type is '%s' but link failed. \n", new_pad_type);
@@ -198,6 +216,69 @@ exit:
         gst_caps_unref(new_pad_caps);
     }
 
-    /* Unreference the sink pad */
-    gst_object_unref(sink_pad);
+    /* Unreference the sink pads */
+    if (sink_pad != nullptr)
+        gst_object_unref(sink_pad);
+}
+
+static bool check_error_elements_created(CustomData &data)
+{
+    bool err_detected = false;
+    if (data.pipeline == nullptr)
+    {
+        g_printerr("Pipeline element could not be created. \n");
+        err_detected = true;
+    }
+    if (data.source == nullptr)
+    {
+        g_printerr("Source element could not be created. \n");
+        err_detected = true;
+    }
+    if (data.video_convert == nullptr)
+    {
+        g_printerr("Video convert element could not be created. \n");
+        err_detected = true;
+    }
+    if (data.audio_convert == nullptr)
+    {
+        g_printerr("Audio convert element could not be created. \n");
+        err_detected = true;
+    }
+    if (data.resample == nullptr)
+    {
+        g_printerr("Resample element could not be created. \n");
+        err_detected = true;
+    }
+    if (data.video_sink == nullptr)
+    {
+        g_printerr("Video sink element could not be created. \n");
+        err_detected = true;
+    }
+    if (data.audio_sink == nullptr)
+    {
+        g_printerr("Audio sink element could not be created. \n");
+        err_detected = true;
+    }
+    return err_detected;
+}
+
+static void retrive_pad_info(GstPad *pad)
+{
+    GstCaps *pad_caps = nullptr;
+    GstStructure *pad_struct = nullptr;
+    const gchar *pad_type = nullptr;
+
+    std::cout << "Trying to retrieve pad info about "<< GST_PAD_NAME(pad) << " pad\n";
+
+    if (gst_pad_has_current_caps(pad))
+    {
+            pad_caps = gst_pad_get_current_caps(pad);
+            pad_struct = gst_caps_get_structure(pad_caps, 0);
+            pad_type = gst_structure_get_name(pad_struct);
+            std::cout << "Compatible pad name is " << pad_type << "\n"; 
+    }
+    else
+    {
+        std::cout << "No current caps for "<< GST_PAD_NAME(pad) << " pad\n";
+    }
 }
